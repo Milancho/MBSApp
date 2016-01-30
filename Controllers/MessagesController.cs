72c79c29084217f6d7026427b804ea20c8d779fe
Milancho <milancho.arsovski@hotmail.com>
2016-01-30 19:46:32 +0100

    MBSApp commit

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MBSApp.Models;
+
+namespace MBSApp.Controllers
+{
+    public class MessagesController : ApiController
+    {
+        Message[] messages = new Message[]
+         {
+            new Message { rMessageID = 1, Subject = "Sport", Body = "Odbojka, vtornik 18 casot"}
+         };
+        
+        public IEnumerable<Message> GetAllMessages()
+        {
+            return messages;
+        }
+
+        public IHttpActionResult GetMessage(int id)
+        {
+            var message = messages.FirstOrDefault((p) => p.rMessageID == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            return Ok(message);
+        }
+    }
+}
